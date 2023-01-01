640be2550ab71fdff6825ddaa46d35227a6e0c9b
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-01-01 19:47:45 -0300

    Catppuccin baby

--- config.def.h
+++ config.def.h
@@ -1,6 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
+#include "themes/catppuccin.h"
+
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
@@ -9,9 +11,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#cdd6f4", "#11111b" },
-	[SchemeSel] = { "#11111b", "#cdd6f4" },
-	[SchemeOut] = { "#000000", "#cdd6f4" },
+	[SchemeNorm] = { normfgcolor, normbgcolor },
+	[SchemeSel]  = { selfgcolor, selbgcolor },
+	[SchemeOut]  = { outfgcolor, outbgcolor },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
---
+++ themes/catppuccin.h
@@ -0,0 +1,6 @@
+static const char normfgcolor[]     = "#cdd6f4";
+static const char normbgcolor[]     = "#11111b";
+static const char selfgcolor[]      = "#11111b";
+static const char selbgcolor[]      = "#cdd6f4";
+static const char outfgcolor[]      = "#000000";
+static const char outbgcolor[]      = "#cdd6f4";
